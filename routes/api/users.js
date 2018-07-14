867c20477b5a43ed455c4b2be506855f8a9d45f3
Alvin van der Kuech <vanderkuech@icloud.com>
2018-07-14 17:33:26 -0400

    configure users route

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,9 +2,30 @@ const express = require("express");
 
 const router = express.Router();
 
+// Load User model
+const User = require("../../models/User");
+
 // @route   GET to api/users/test
 // @desc    Tests users route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
+
+// @route   GET to api/users/register
+// @desc    Register user
+// @access  Public
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) res.status(400).json({ email: "Email already exists" });
+    else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar: avatar,
+        password: req.body.password
+      });
+    }
+  });
+});
 
 module.exports = router;
