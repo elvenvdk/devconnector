04f1c59bc38596c6543d5b932cc1c2297b597bf2
Alvin van der Kuech <vanderkuech@icloud.com>
2018-07-25 16:24:47 -0400

    Add profileReducer, types

---
+++ client/src/reducers/profileReducer.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+  profile: null,
+  profiles: null,
+  loading: false
+};
+
+const profileReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
