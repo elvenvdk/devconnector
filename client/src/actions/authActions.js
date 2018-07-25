551f7288fa730bcd0891b1a4ffa2116b0456223e
Alvin van der Kuech <vanderkuech@icloud.com>
2018-07-25 09:14:14 -0400

    add push to login history push - authactions

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import * as types from "./types";
 
-export const registerUser = userData => dispatch => {
+export const registerUser = (userData, history) => dispatch => {
   axios
     .post("/api/users/register", userData)
-    .then(res => console.log(res.data))
+    .then(res => history.push("/login"))
     .catch(err =>
       dispatch({
         type: types.GET_ERRORS,
