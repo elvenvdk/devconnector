2568674eebf46355274c32da60876c61048e9376
Alvin van der Kuech <vanderkuech@icloud.com>
2018-08-01 09:36:07 -0400

    Add add education component and functionality

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -5,6 +5,8 @@ import PropTypes from "prop-types";
 import { getCurrentProfile, deleteAccount } from "../../actions/profileActions";
 import Spinner from "../common/Spinner";
 import ProfileActions from "./ProfileActions";
+import Experience from "./Experience";
+import Education from "./Education";
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -30,6 +32,8 @@ class Dashboard extends Component {
               Welcome <Link to={profile.handle}>{user.name}</Link>
             </p>
             <ProfileActions />
+            <Experience experience={profile.experience} />
+            <Education education={profile.education} />
             {/* TODO: exp and edu */}
             <div style={{ marginBottom: "60px" }} />
             <button onClick={this.onDeleteClick} className="btn btn-danger">
