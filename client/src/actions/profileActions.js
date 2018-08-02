19da20abf52bad6d17ee051fdba93e625f11b6ed
Alvin van der Kuech <vanderkuech@icloud.com>
2018-08-02 12:32:29 -0400

    Add user profile header, about, credentials component and functionality

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -20,6 +20,24 @@ export const getCurrentProfile = () => dispatch => {
     );
 };
 
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res =>
+      dispatch({
+        type: types.GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: types.GET_PROFILE,
+        payload: null
+      })
+    );
+};
+
 // Create Profile
 export const createProfile = (profileData, history) => dispatch => {
   axios
