45ff9358a768a1efafe669c5615e458f8953d148
Alvin van der Kuech <vanderkuech@icloud.com>
2018-08-03 16:29:24 -0400

    Add comments components and functionality

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import * as types from "./types";
 
 export const addPost = postData => dispatch => {
+  dispatch(clearErrors());
   axios
     .post("/api/posts", postData)
     .then(res =>
@@ -18,3 +19,126 @@ export const addPost = postData => dispatch => {
       })
     );
 };
+
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get("/api/posts")
+    .then(res =>
+      dispatch({
+        type: types.GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: types.GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+export const getPost = id => dispatch => {
+  axios
+    .get(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: types.GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: types.GET_POST,
+        payload: null
+      })
+    );
+};
+
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: types.DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: types.GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Add Like
+export const addLike = id => dispatch => {
+  axios
+    .post(`/api/posts/like/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: types.GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Remove Like
+export const removeLike = id => dispatch => {
+  axios
+    .post(`/api/posts/unlike/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: types.GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+export const addComment = (id, commentData) => dispatch => {
+  dispatch(clearErrors());
+  axios
+    .post(`/api/posts/comment/${id}`, commentData)
+    .then(res =>
+      dispatch({
+        type: types.GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: types.GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+export const deleteComment = (postId, commentId) => dispatch => {
+  axios
+    .delete(`/api/posts/comment/${postId}/${commentId}`)
+    .then(res =>
+      dispatch({
+        type: types.GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: types.GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Profile loading
+export const setPostLoading = () => ({
+  type: types.POST_LOADING
+});
+
+// Clear profile
+export const clearErrors = () => ({
+  type: types.CLEAR_ERRORS
+});
--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -7,6 +7,33 @@ const INITIAL_STATE = {
 
 const postReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case types.ADD_POST:
+      return {
+        ...state,
+        posts: [action.payload, ...state.posts]
+      };
+    case types.GET_POSTS:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false
+      };
+    case types.GET_POST:
+      return {
+        ...state,
+        post: action.payload,
+        loading: false
+      };
+    case types.POST_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case types.DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== action.payload)
+      };
     default:
       return state;
   }
